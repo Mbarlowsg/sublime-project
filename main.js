8296580355725521b822b4df68254df8cf2aa24b
lewam075@newschool.edu <148263350+MilesLewandowski@users.noreply.github.com>
2023-10-17 14:12:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 /** @format */
+let count = 0;
+let dayCount = 555;
+let monthCount = 0;
+let yearCount = 0;
 
 window.onload = () => {
-	console.log("Hello World");
+	setInterval(() => {
+		document.getElementById("count").innerText = `Count: ${count}`;
+		count++;
+	}, dayCount);
 };
+
+function clearCount() {
+	count = 0;
+}
+
+function refreshCount() {}
