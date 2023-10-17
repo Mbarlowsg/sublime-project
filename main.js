7d29b565050eb1b410437adfaa8d2fe0b0786a46
Mbarlowsg <130187725+Mbarlowsg@users.noreply.github.com>
2023-10-17 14:10:01 -0400

    Added Clear button functionality

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 /** @format */
 let count = 0;
+
 window.onload = () => {
 	setInterval(() => {
-		document.getElementById("count").innerText = `Count: ${count}`;
 		count++;
+		console.log(count);
+		refreshCount();
 	}, 555);
 };
 
 function clearCount() {
 	count = 0;
+	refreshCount();
 }
 
-function refreshCount() {}
+function refreshCount() {
+	document.getElementById("count").innerText = `Count: ${count}`;
+}
