c969c1b790abed7c5b1b5a4ca8eefee31cd21c4a
Mbarlowsg <130187725+Mbarlowsg@users.noreply.github.com>
2023-10-19 17:08:09 -0400

    First version of final page - with styling

--- Experimental Pages/Final Version/main.js
+++ Experimental Pages/Final Version/main.js
@@ -1,12 +1,12 @@
 /** @format */
-let count = 0;
+let count = "000000000" + "1";
 // Rate of count increase in ms
 let deathRate = 555;
 let interval;
 
 window.onload = () => {
 	setInterval(increaseCount, 555);
-	setTimeout(fadeInMessageOne, 10000);
+	setTimeout(fadeInMessageOne, 7000);
 	setTimeout(fadeInMessageTwo, 12000);
 };
 
@@ -16,9 +16,17 @@ function increaseCount() {
 }
 
 function fadeInMessageOne(message) {
-	document.getElementById("message-one").classList.add("fade-in-text");
+	document.getElementById("message-1").classList.add("fade-in-text");
+	document.getElementById("message-1").classList.remove("hide-text");
 }
 
 function fadeInMessageTwo() {
-	document.getElementById("message-two").classList.add("fade-in-text");
+	document.getElementById("message-2").classList.add("fade-in-text");
+	document.getElementById("message-2").classList.remove("hide-text");
+}
+
+function refreshCount() {
+	for (let index = 0; index < count.length; index++) {
+		console.log(index);
+	}
 }
