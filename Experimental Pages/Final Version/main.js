c94e3e4f9d3124d8e0bd040804d5d7928d8bb6ed
Mbarlowsg <130187725+Mbarlowsg@users.noreply.github.com>
2023-10-19 16:01:34 -0400

    update

--- Experimental Pages/Final Version/main.js
+++ Experimental Pages/Final Version/main.js
@@ -6,9 +6,19 @@ let interval;
 
 window.onload = () => {
 	setInterval(increaseCount, 555);
+	setTimeout(fadeInMessageOne, 10000);
+	setTimeout(fadeInMessageTwo, 12000);
 };
 
 function increaseCount() {
 	count++;
 	document.getElementById("count").innerText = `${count}`;
+}
+
+function fadeInMessageOne(message) {
+	document.getElementById("message-one").classList.add("fade-in-text");
+}
+
+function fadeInMessageTwo() {
+	document.getElementById("message-two").classList.add("fade-in-text");
 }
